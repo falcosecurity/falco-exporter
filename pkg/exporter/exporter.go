4e104725f4725815cafeb30d8a38f79f672abbb8
Leonardo Grasso <me@leonardograsso.com>
2019-10-05 22:16:02 +0200

    feat(pkg/exporter): initial exporter package

---
+++ pkg/exporter/exporter.go
@@ -0,0 +1,47 @@
+package exporter
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/falcosecurity/client-go/pkg/api/output"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	eventsCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "falco_events",
+		},
+		[]string{
+			"rule",
+			"priority",
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(eventsCounter)
+}
+
+// Subscribe to a ServiceClient to receive a stream of falco output events.
+func Subscribe(ctx context.Context, outputClient output.ServiceClient) error {
+	// Keepalive true means that the client will wait indefinitely for new events to come
+	// Use keepalive false if you only want to receive the accumulated events and stop
+	fcs, err := outputClient.Subscribe(ctx, &output.Request{Keepalive: true})
+	if err != nil {
+		return err
+	}
+
+	for {
+		res, err := fcs.Recv()
+		if err != nil {
+			return err
+		}
+
+		eventsCounter.With(prometheus.Labels{
+			"rule":     res.Rule,
+			"priority": fmt.Sprintf("%d", res.Priority),
+		}).Inc()
+	}
+}
