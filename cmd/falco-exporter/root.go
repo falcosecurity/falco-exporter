246bbc3c79bd383fb2bebf2758525bf94d267cfa
Leonardo Grasso <me@leonardograsso.com>
2020-05-26 13:48:24 +0200

    feat: add `--timeout`, enable exponential backoff, expose readiness endpoint
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- cmd/falco-exporter/root.go
+++ cmd/falco-exporter/root.go
@@ -5,19 +5,30 @@ import (
 	goflag "flag"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/falcosecurity/client-go/pkg/client"
 	"github.com/falcosecurity/falco-exporter/pkg/exporter"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spf13/pflag"
+	"google.golang.org/grpc"
 )
 
 func main() {
 
-	addr := ""
+	var addr string
 	pflag.StringVar(&addr, "listen-address", ":9376", "address on which to expose the Prometheus metrics")
 
-	config := &client.Config{}
+	var timeout time.Duration
+	pflag.DurationVar(&timeout, "timeout", time.Minute*2, "timeout for initial gRPC connection")
+
+	config := &client.Config{
+		DialOptions: []grpc.DialOption{
+			// Instruct `client.NewForConfig` to wait until the underlying connection is up,
+			// the dialer will use the default gRPC backoff if needed.
+			grpc.WithBlock(),
+		},
+	}
 	pflag.StringVar(&config.Hostname, "client-hostname", "localhost", "hostname for connecting to a Falco gRPC server")
 	pflag.Uint16Var(&config.Port, "client-port", 5060, "port for connecting to a Falco gRPC server")
 	pflag.StringVar(&config.CertFile, "client-cert", "/etc/falco/certs/client.crt", "cert file path for connecting to a Falco gRPC server")
@@ -27,27 +38,46 @@ func main() {
 	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 	pflag.Parse()
 
-	c, err := client.NewForConfig(config)
+	go serveMetrics(addr)
+
+	log.Printf("connecting to gRPC server %s:%d", config.Hostname, config.Port)
+	// cancel the pending connection after timeout is reached
+	dialerCtx, cancelTimeout := context.WithTimeout(context.Background(), timeout)
+	c, err := client.NewForConfig(dialerCtx, config)
 	if err != nil {
-		log.Fatalf("unable to connect: %v", err)
+		log.Fatalf("gRPC: %v\n", err)
 	}
 	defer c.Close()
 	outputClient, err := c.Output()
 	if err != nil {
-		log.Fatalf("unable to obtain an output client: %v", err)
+		log.Fatalf("gRPC: %v\n", err)
 	}
 
-	ctx := context.Background()
-	go func() {
-		if err := exporter.Subscribe(ctx, outputClient); err != nil {
-			if err != nil {
-				log.Fatalf("exporter error: %v", err)
-			}
-		}
-	}()
+	log.Println("connected to gRPC server, subscribing events stream")
 
+	recv, err := exporter.Subscribe(context.Background(), outputClient)
+	if err != nil {
+		log.Fatalf("gRPC: %v\n", err)
+	}
+
+	cancelTimeout()
+	enableReadiness()
+	log.Println("ready, receiving events")
+	if err := recv(); err != nil {
+		log.Fatalf("gRPC: %v\n", err)
+	}
+}
+
+func serveMetrics(addr string) {
 	http.Handle("/metrics", promhttp.Handler())
-	if err = http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("%v", err)
+	log.Printf("listening on %s/metrics\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("server: %v", err)
 	}
+}
+
+func enableReadiness() {
+	http.HandleFunc("/readiness", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 }
