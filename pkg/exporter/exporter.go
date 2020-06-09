4278969a02dbf0cadca0682f2321986a3764a967
Leonardo Grasso <me@leonardograsso.com>
2020-06-09 18:58:52 +0200

    update: switch to bidirectional gRPC
    BREAKING CHANGE: switching to new bidi gRCP API makes `falco-exporter` incompatible with Falco version `0.23.0` and older. Moreover, `exporter.Subscribe` function has been removed.
    
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -3,14 +3,12 @@ package exporter
 import (
 	"context"
 	"fmt"
+	"time"
 
-	"github.com/falcosecurity/client-go/pkg/api/output"
+	"github.com/falcosecurity/client-go/pkg/api/outputs"
+	"github.com/falcosecurity/client-go/pkg/client"
 	"github.com/prometheus/client_golang/prometheus"
-	"google.golang.org/grpc"
 )
-
-// A RecvFunc waits for subscribed events and forwards to metric counters.
-type RecvFunc func() error
 
 var (
 	eventsCounter = prometheus.NewCounterVec(
@@ -32,46 +30,31 @@ func init() {
 	prometheus.MustRegister(eventsCounter)
 }
 
-// Subscribe to a ServiceClient to receive a stream of Falco output events.
-// If success, it returns a RecvFunc that can be used.
-// To stop the subscription, use Cancel() on the context provided.
-func Subscribe(ctx context.Context, outputClient output.ServiceClient, opts ...grpc.CallOption) (RecvFunc, error) {
-
-	// Keepalive true means that the client will wait indefinitely for new events to come
-	fcs, err := outputClient.Subscribe(ctx, &output.Request{Keepalive: true}, opts...)
-	if err != nil {
-		return nil, err
+// Forward processes a single *outputs.Response and forwards the event to the metric counters.
+func Forward(res *outputs.Response) error {
+	labels := prometheus.Labels{
+		"rule":         res.Rule,
+		"priority":     fmt.Sprintf("%d", res.Priority),
+		"hostname":     res.Hostname,
+		"source":       res.Source.String(),
+		"k8s_ns_name":  "",
+		"k8s_pod_name": "",
 	}
 
-	return func() error {
-		for {
-			res, err := fcs.Recv()
-			if err != nil {
-				return err
-			}
+	// Ensure OutputFields are enabled
+	if res.OutputFields != nil {
+		labels["k8s_ns_name"] = res.OutputFields["k8s.ns.name"]
+		labels["k8s_pod_name"] = res.OutputFields["k8s.pod.name"]
+	}
 
-			labels := prometheus.Labels{
-				"rule":         res.Rule,
-				"priority":     fmt.Sprintf("%d", res.Priority),
-				"hostname":     res.Hostname,
-				"source":       res.Source.String(),
-				"k8s_ns_name":  "",
-				"k8s_pod_name": "",
-			}
+	eventsCounter.With(labels).Inc()
+	return nil
+}
 
-			// Ensure OutputFields are enabled
-			if res.OutputFields != nil {
-				labels["k8s_ns_name"] = res.OutputFields["k8s.ns.name"]
-				labels["k8s_pod_name"] = res.OutputFields["k8s.pod.name"]
-			}
-
-			eventsCounter.With(labels).Inc()
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			default:
-			}
-		}
-	}, nil
+// Watch allows to watch and process a stream of *outputs.Response from a given outputs.Service_SubClient.
+// The timeout parameter specifies the frequency of the watch operation.
+//
+// It waits until the stream is closed, the context is cancelled or an error occured.
+func Watch(ctx context.Context, sc outputs.Service_SubClient, timeout time.Duration) error {
+	return client.OutputsWatch(ctx, sc, Forward, timeout)
 }
